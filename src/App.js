eab78434047b3844eaf8192603ee8555ff9e7dfd
Andrew Zaikin <zaikin.andry@gmail.com>
2022-01-12 01:27:20 +0300

    add modals

--- src/App.js
+++ src/App.js
@@ -5,8 +5,21 @@ import {
   Route,
   Link
 } from "react-router-dom";
-
+import Modal from 'react-modal';
 import styles from './App.module.scss';
+
+Modal.setAppElement('#root');
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+  },
+};
+
 export default function App() {
   return (
     <Router>
@@ -38,7 +51,43 @@ export default function App() {
 }
 
 function Home() {
-  return <h2>Home</h2>;
+  let subtitle;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = '#f00';
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+  return (
+    <div>
+      <button onClick={openModal}>Open Modal</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
+        <button onClick={closeModal}>close</button>
+        <div>I am a modal</div>
+        <form>
+          <input />
+          <button>tab navigation</button>
+          <button>stays</button>
+          <button>inside</button>
+          <button>the modal</button>
+        </form>
+      </Modal>
+    </div>
+  )
 }
 
 function About() {
