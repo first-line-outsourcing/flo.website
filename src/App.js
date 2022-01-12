204c5bfc7d32e3119572f36f1447dd6ce5dee2d9
Andrew Zaikin <zaikin.andry@gmail.com>
2022-01-12 23:26:12 +0300

    add nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
-import styles from './App.module.scss';
+import React from 'react';
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
+import './App.scss';
+import logo from './images/Logo.svg';
+
 import About from './pages/About';
 import Case from './pages/Case';
 import Cases from './pages/Cases';
@@ -21,55 +18,69 @@ import Technology from './pages/Technology';
 export default function App() {
   return (
     <Router>
-      <div>
-        <ul className="nav nav-pills nav-justified">
-          <li className="nav-item">
-            <Link className="nav-link" aria-current="page" to="/">Home</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" aria-current="page" to="/components">Components</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" aria-current="page" to="/about">About</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" aria-current="page" to="/cases">Cases</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" aria-current="page" to="/case">Case</Link>
-          </li>
-          <li className="nav-item">
-          <Link className="nav-link" aria-current="page" to="/contacts">Contacts</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" aria-current="page" to="/services">Services</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" aria-current="page" to="/service">Service</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" aria-current="page" to="/technologies">Technologies</Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" aria-current="page" to="/technology">Technology</Link>
-          </li>
+      <div className={'container'}>
+        <nav className='navbar navbar-expand-lg'>
+          <div className='container-fluid'>
+            <Link className={'navbar-brand'} aria-current='page' to='/'>
+              <img src={logo} alt='' width={76} />
+            </Link>
+            <button className='navbar-toggler' type='button' data-bs-toggle='collapse'
+                    data-bs-target='#navbarTogglerDemo02' aria-controls='navbarTogglerDemo02' aria-expanded='false'
+                    aria-label='Toggle navigation'>
+              <span className='navbar-toggler-icon'> </span>
+            </button>
+            <div className={'d-flex'}>
+              <div className='collapse navbar-collapse' id='navbarTogglerDemo02'>
+                <ul className={'navbar-nav me-auto mb-2 mb-lg-0'}>
+                  <li className='nav-item'>
+                    <Link className='nav-link' aria-current='page' to='/services'>Our services</Link>
+                  </li>
+                  <li className='nav-item'>
+                    <Link className='nav-link' aria-current='page' to='/technologies'>Technologies</Link>
+                  </li>
+                  <li className='nav-item'>
+                    <Link className='nav-link' aria-current='page' to='/components'>Components</Link>
+                  </li>
+                  <li className='nav-item'>
+                    <Link className='nav-link' aria-current='page' to='/cases'>Portfolio</Link>
+                  </li>
+                  <li className='nav-item'>
+                    <Link className='nav-link' aria-current='page' to='/about'>About</Link>
+                  </li>
+                  <li className='nav-item'>
+                    <Link className='nav-link' aria-current='page' to='/contacts'>Contacts</Link>
+                  </li>
+                  {/*<li className='nav-item'>*/}
+                  {/*  <Link className='nav-link' aria-current='page' to='/case'>Case</Link>*/}
+                  {/*</li>*/}
+                  {/*<li className='nav-item'>*/}
+                  {/*  <Link className='nav-link' aria-current='page' to='/service'>Service</Link>*/}
+                  {/*</li>*/}
+                  {/*<li className='nav-item'>*/}
+                  {/*  <Link className='nav-link' aria-current='page' to='/technology'>Technology</Link>*/}
+                  {/*</li>*/}
+                </ul>
+                <button type={'button'} className='btn btn-accent'>Contact us</button>
+              </div>
+            </div>
+          </div>
 
-        </ul>
+        </nav>
 
         {/* A <Routes> looks through its children <Route>s and
          renders the first one that matches the current URL. */}
         <Routes>
-          <Route path="/about" element={<About />} />
-          <Route path="/case" element={<Case />} />
-          <Route path="/cases" element={<Cases />} />
-          <Route path="/components" element={<Components />} />
-          <Route path="/contacts" element={<Contacts />} />
-          <Route path="/process" element={<Process />} />
-          <Route path="/service" element={<Service />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/technologies" element={<Technologies />} />
-          <Route path="/technology" element={<Technology />} />
-          <Route path="/" element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/case' element={<Case />} />
+          <Route path='/cases' element={<Cases />} />
+          <Route path='/components' element={<Components />} />
+          <Route path='/contacts' element={<Contacts />} />
+          <Route path='/process' element={<Process />} />
+          <Route path='/service' element={<Service />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/technologies' element={<Technologies />} />
+          <Route path='/technology' element={<Technology />} />
+          <Route path='/' element={<Home />} />
         </Routes>
       </div>
     </Router>
