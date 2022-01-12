8e8e52ca550ece9ccac1651c10297c1f1f97c058
Andrew Zaikin <zaikin.andry@gmail.com>
2022-01-13 01:45:01 +0300

    add heading

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,5 +1,18 @@
 import React from "react";
+import './Services.scss';
 
 export default function Services() {
-  return <h2>Home</h2>;
+  return (
+    <div className='services'>
+      <div className="heading">
+        <div className="container">
+          <div className="row">
+            <div className="col-8">
+              <h1>Full-cycle development for your business needs</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/pages/Technologies.js
+++ src/pages/Technologies.js
@@ -1,5 +1,17 @@
 import React from "react";
 
 export default function Technologies() {
-  return <h2>Home</h2>;
+  return (
+    <div className='services'>
+      <div className="heading">
+        <div className="container">
+          <div className="row">
+            <div className="col-8">
+              <h1>Full-cycle development for your business needs</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
