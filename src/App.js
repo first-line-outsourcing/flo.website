6a9d0ac9a257f06d07315e280d36760c5041d3db
Andrew Zaikin <zaikin.andry@gmail.com>
2022-01-13 01:14:20 +0300

    fix nav

--- src/App.js
+++ src/App.js
@@ -19,54 +19,57 @@ import Technology from './pages/Technology';
 export default function App() {
   return (
     <Router>
-      <div className="container">
-        <nav className="navbar navbar-expand-lg">
-          <div className="container-fluid">
-            <Link className="navbar-brand" aria-current="page" to="/">
-              <img src={logo} alt="" width={76} />
-            </Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false"
-                    aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"> </span>
-            </button>
-            <div className="d-flex">
-              <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li className="nav-item">
-                    <Link className="nav-link" aria-current="page" to="/services">Our services</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" aria-current="page" to="/technologies">Technologies</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" aria-current="page" to="/components">Components</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" aria-current="page" to="/cases">Portfolio</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" aria-current="page" to="/about">About</Link>
-                  </li>
-                  <li className="nav-item">
-                    <Link className="nav-link" aria-current="page" to="/contacts">Contacts</Link>
-                  </li>
-                  {/*<li className='nav-item'>*/}
-                  {/*  <Link className='nav-link' aria-current='page' to='/case'>Case</Link>*/}
-                  {/*</li>*/}
-                  {/*<li className='nav-item'>*/}
-                  {/*  <Link className='nav-link' aria-current='page' to='/service'>Service</Link>*/}
-                  {/*</li>*/}
-                  {/*<li className='nav-item'>*/}
-                  {/*  <Link className='nav-link' aria-current='page' to='/technology'>Technology</Link>*/}
-                  {/*</li>*/}
-                </ul>
-                <button type={'button'} className="btn btn-accent">Contact us</button>
+      <header>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg">
+            <div className="container-fluid">
+              <Link className="navbar-brand" aria-current="page" to="/">
+                <img src={logo} alt="" width={76} />
+              </Link>
+              <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                      data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false"
+                      aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"> </span>
+              </button>
+              <div className="d-flex">
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                  <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                      <Link className="nav-link" aria-current="page" to="/services">Our services</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" aria-current="page" to="/technologies">Technologies</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" aria-current="page" to="/components">Components</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" aria-current="page" to="/cases">Portfolio</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" aria-current="page" to="/about">About</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" aria-current="page" to="/contacts">Contacts</Link>
+                    </li>
+                    {/*<li className='nav-item'>*/}
+                    {/*  <Link className='nav-link' aria-current='page' to='/case'>Case</Link>*/}
+                    {/*</li>*/}
+                    {/*<li className='nav-item'>*/}
+                    {/*  <Link className='nav-link' aria-current='page' to='/service'>Service</Link>*/}
+                    {/*</li>*/}
+                    {/*<li className='nav-item'>*/}
+                    {/*  <Link className='nav-link' aria-current='page' to='/technology'>Technology</Link>*/}
+                    {/*</li>*/}
+                  </ul>
+                  <button type={'button'} className="btn btn-accent">Contact us</button>
+                </div>
               </div>
             </div>
-          </div>
-        </nav>
-      </div>
+          </nav>
+        </div>
+      </header>
+
 
       {/* A <Routes> looks through its children <Route>s and
        renders the first one that matches the current URL. */}
