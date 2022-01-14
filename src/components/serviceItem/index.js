50d165ff46c3ecf00feb129005d0df8b82925159
vchorba-flo <vchorba@flo.team>
2022-01-14 19:11:44 +0300

    Add link to items

--- src/components/serviceItem/index.js
+++ src/components/serviceItem/index.js
@@ -2,9 +2,9 @@ import "./styles.scss"
 import arrow from "../../images/services/arrow.svg"
 import arrowMobile from "../../images/services/arrowMobile.svg"
 
-export default function ServiceItem({title, description, img}) {
+export default function ServiceItem({title, description, img, link}) {
   return (
-    <div className="item row">
+    <a href={link} className="item row">
       <div className="col-md-auto">
         <h4>{title}</h4>
       </div>
@@ -16,6 +16,6 @@ export default function ServiceItem({title, description, img}) {
         <img className="image" src={img} alt=""/>
       </div>
       <div className="arrowMobile"><img src={arrowMobile} alt=""/></div>
-    </div>
+    </a>
   )
 }
