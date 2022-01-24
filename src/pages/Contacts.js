247b143b1942b722c7132fb92ef57f71d0d01030
akalinichenko-flo <akalinichenko@flo.team>
2022-01-24 11:00:10 +0300

    feat: add contacts page and mobile adaptation

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -1,5 +1,39 @@
 import React from "react";
+import point from '../images/contacts/point.svg'
+import phone from '../images/contacts/phone.svg'
+import message from '../images/contacts/message.png'
+import map from '../images/contacts/map.png'
+import './Contacts.scss'
 
 export default function Contacts() {
-  return <h2>Home</h2>;
+    return <div className={'contacts'}>
+        <div className={'container'}>
+            <div className={'title'}>Contacts</div>
+            <div className={'mapAndInfo'}>
+                <div className={'info'}>
+                    
+                        <div className={'point'}><img src={point} alt={''}/></div>
+                        <div className={'address'}><div>Address</div> <div>Krasnodar, Russian Federation</div></div>
+                    
+                    
+                        <div className={'phone'}><img src={phone} alt={''}/></div>
+                        <div className={'number'}>
+                            <div>Call Us</div>
+                            <div> +7 996 408 39 86</div>
+                        </div>
+              
+                    <div className={'message'}><img src={message} alt={''}/></div>
+                        <div className={'mail'}>
+                        <div>For Support Enquiries</div>
+                        <div>info@rmeng.ru</div>
+                        </div>
+                    
+                </div>
+                
+                <div className={'map'}>
+                    <div><img src={map} alt={''}/></div>
+                </div>
+            </div>
+        </div>
+    </div>
 }
