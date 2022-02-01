4126faf8956d4bc8cd80e5ce36b6af776e669bd8
akalinichenko-flo <akalinichenko@flo.team>
2022-02-01 19:34:10 +0300

    refactor: some refactor

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -2,37 +2,39 @@ import React from "react";
 import point from '../images/contacts/point.svg'
 import phone from '../images/contacts/phone.svg'
 import message from '../images/contacts/message.png'
-import map from '../images/contacts/map.png'
 import './Contacts.scss'
 
 export default function Contacts() {
-    return <div className={'contacts'}>
-        <div className={'container'}>
-            <div className={'title'}>Contacts</div>
-            <div className={'mapAndInfo'}>
-                <div className={'info'}>
-                    
-                        <div className={'point'}><img src={point} alt={''}/></div>
-                        <div className={'address'}><div>Address</div> <div>Krasnodar, Russian Federation</div></div>
-                    
-                        <div className={'phone'}><img src={phone} alt={''}/></div>
-                        <div className={'number'}>
-                            <div>Call Us</div>
-                            <div> +7 996 408 39 86</div>
-                        </div>
-              
-                    <div className={'message'}><img src={message} alt={''}/></div>
-                        <div className={'mail'}>
-                        <div>For Support Enquiries</div>
-                        <div>info@rmeng.ru</div>
-                        </div>
-                    
-                </div>
-                
-                <div className={'mapContainer'}>
-                    <div className={'map'}/>
-                </div>
-            </div>
+  return <div className='contacts'>
+    <div className='container'>
+      <div className='contactsTitle'>Contacts</div>
+      <div className='mapAndInfo'>
+        <div className='info'>
+
+          <img src={point} alt='' className='point'/>
+          <div className='address'>
+            <span>Address</span>
+            <span className='block'>Krasnodar, Russian Federation</span>
+          </div>
+
+          <img src={phone} alt='' className='phone'/>
+          <div className='number'>
+            <span>Call Us</span>
+            <span className='block'> +7 996 408 39 86</span>
+          </div>
+
+          <img src={message} alt='' className='message'/>
+          <div className='mail'>
+            <span>For Support Enquiries</span>
+            <span className='block'>info@rmeng.ru</span>
+          </div>
+
         </div>
+
+        <div className='mapContainer'>
+          <div className='map'/>
+        </div>
+      </div>
     </div>
+  </div>
 }
