3245dec88a743ab3382f4d8f756632458da842fb
vchorba-flo <vchorba@flo.team>
2022-02-07 15:24:11 +0300

    Contact page now has a map instead of a picture

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -2,7 +2,6 @@ import React from "react";
 import point from '../images/contacts/point.svg'
 import phone from '../images/contacts/phone.svg'
 import message from '../images/contacts/message.svg'
-import map from '../images/contacts/map.png'
 import './Contacts.scss'
 
 export default function Contacts() {
@@ -33,7 +32,8 @@ export default function Contacts() {
         </div>
 
         <div className='mapContainer'>
-          <img src={map} alt='' className='map'/>
+          <iframe className='map' title="map"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2819.608031310213!2d38.96634605195044!3d45.03288187899546!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40f04fa3cbf68c8d%3A0x79321c0649b7567e!2sOktyabr&#39;skaya%20Ulitsa%2C%20141%2C%20Krasnodar%2C%20Krasnodarskiy%20kray%2C%20350000!5e0!3m2!1sen!2sru!4v1644235175636!5m2!1sen!2sru"/>
         </div>
       </div>
     </div>
