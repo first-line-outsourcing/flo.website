e239541e2e40025142e541f63b0f587725578b3a
Kirill Khoroshilov <khoroshilov@flo.team>
2022-05-07 05:18:21 +0300

    chore: update comments

--- src/components/buttons/HumburgerButton.jsx
+++ src/components/buttons/HumburgerButton.jsx
@@ -3,6 +3,15 @@ import React from 'react';
 import classNames from 'classnames';
 import * as styles from './HumburgerButton.module.css';
 
+
+/**
+ * Humburger button
+ *
+ * @param {React.ButtonHTMLAttributes} props Props
+ * @param {boolean} [props.open] Set open state
+ * @returns {JSX.Element}
+ * @constructor
+ */
 export function HumburgerButton(props) {
   return (
     <button {...props} className={classNames(styles.root, props.className, {
