00acb360d6343969711edb7c6682f3cae35a05bd
Kirill Khoroshilov <khoroshilov@flo.team>
2022-05-20 21:42:57 +0300

    feat: hide contacts page

---
+++ src/pages/_contacts.jsx
@@ -0,0 +1,6 @@
+// Remove _ at the beginning of the file if you want this page to be available for users
+// As Andrew said this page must not be public yet
+import * as React from "react"
+import {Contacts} from '../pages-views/contacts/Contacts';
+
+export default Contacts
--- src/pages/contacts.jsx
+++
@@ -1,4 +0,0 @@
-import * as React from "react"
-import {Contacts} from '../pages-views/contacts/Contacts';
-
-export default Contacts
