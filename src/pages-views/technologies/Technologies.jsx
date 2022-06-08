d905a1897fa0e19a227e0b2bc04289144ec4d794
Kirill Khoroshilov <khoroshilov@flo.team>
2022-06-08 17:32:41 +0300

    feat: update text

--- src/pages-views/technologies/Technologies.jsx
+++ src/pages-views/technologies/Technologies.jsx
@@ -34,7 +34,7 @@ export function Technologies(props) {
               <Card
                 icon="js"
                 name="Node.js"
-                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet."
+                description="A single-threaded, open-source, cross-platform runtime environment for building fast and scalable server-side and networking applications."
               />
             </li>
             <li>
@@ -48,77 +48,84 @@ export function Technologies(props) {
               <Card
                 icon="ts-box"
                 name="TypeScript"
-                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet."
+                description="A strongly typed programming language that builds on JavaScript, giving you better tooling at any scale."
               />
             </li>
             <li>
               <Card
                 icon="angular"
                 name="Angular | NGXS"
-                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet."
+                description="A most potent, component-based, open-source framework for building scalable web applications written in TypeScript. "
               />
             </li>
             <li>
               <Card
                 icon="react"
                 name="React | MobX"
-                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet."
+                description="The main purpose of React is to be fast, scalable, and simple. It works only on user interfaces in the application. This corresponds to the view in the MVC template."
               />
             </li>
             <li>
               <Card
                 icon="sls-framework"
                 name="Serverless Framework"
-                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet."
+                description="An open source tool available for building, packaging and deploying serverless applications across multiple cloud providers and platforms like AWS, GCP, Azure, Kubernetes, etc."
               />
             </li>
             <li>
               <Card
                 icon="express"
                 name="Express | Sails | Nest"
-                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet."
+                description="Node.js frameworks designed to build API's web applications cross-platform mobile apps quickly and make Node.js easy."
               />
             </li>
             <li>
               <Card
                 icon="aws"
                 name="Amazon Web Services"
-                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet."
+                description="A comprehensive, evolving cloud computing platform provided by Amazon that includes a mixture of infrastructure as a service (IaaS), platform as a service (PaaS) and packaged software as a service (SaaS) offerings."
+              />
+            </li>
+            <li>
+              <Card
+                icon="aws"
+                name="AWS SAM/CDK"
+                description="Open-source software development frameworks to define your cloud application resources using familiar programming languages for Amazon Web Services."
               />
             </li>
             <li>
               <Card
                 icon="gcp"
                 name="Google Cloud Platform"
-                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet."
+                description="A public cloud vendor that offers a suite of computing services to do everything from data management to delivering web and video over the web to AI and machine learning tools."
               />
             </li>
             <li>
               <Card
                 icon="azure"
                 name="Microsoft Azure"
-                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet."
+                description="A cloud computing platform and an online portal that allows you to access and manage cloud services and resources provided by Microsoft."
               />
             </li>
             <li>
               <Card
                 icon="ibm"
                 name="IBM Cloud"
-                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet."
+                description="The most open and secure public cloud platform for business, a next-generation hybrid multicloud platform, advanced data and AI capabilities, and deep enterprise expertise across 20 industries."
               />
             </li>
             <li>
               <Card
                 icon="postgresql"
                 name="PostgreSQL | MySQL | Aurora"
-                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet."
+                description="Most popular and powerful databases specifically used by business professionals or program developers for administering, updating, maintaining and manipulating the databases or tables that are used for business decision-making."
               />
             </li>
             <li>
               <Card
                 icon="flutter"
                 name="Flutter | GetIt"
-                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet."
+                description="An open source framework by Google for building beautiful, natively compiled, multi-platform applications from a single codebase."
               />
             </li>
             <li>
@@ -139,14 +146,14 @@ export function Technologies(props) {
               <Card
                 icon="dart"
                 name="Dart"
-                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet."
+                description="Dart is a client-optimized language for developing fast apps on any platform. Its goal is to offer the most productive programming language for multi-platform development, paired with a flexible execution runtime platform for app frameworks."
               />
             </li>
             <li>
               <Card
                 icon="electron"
                 name="Electron"
-                description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet."
+                description="a framework for building desktop applications using JavaScript, HTML, and CSS. By embedding Chromium and Node.js into its binary, Electron allows you to maintain one JavaScript codebase and create cross-platform apps that work on Windows, macOS, and Linux."
               />
             </li>
           </ul>
